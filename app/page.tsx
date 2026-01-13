32dbeffaf39d73ee911d0c8b3e87e7f75ebd2854
Calvin Silva <calvin.silva@clinia.io>
2026-01-13 18:39:14 -0300

    melhoria do home

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,44 @@
+"use client";
+
 import Image from "next/image";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
+
+import { useState } from "react";
+
 
 export default function Home() {
+  const router = useRouter()
+
+  const [usuario, setUsuario] = useState("")
+  const [senha, setSenha] = useState("")
+
+  function handleEntrar() {
+    if (!usuario || !senha) {
+      alert("Preencha todos os campos")
+      return;
+    }
+    router.push("/dashboard")
+  }
+
+
   return (
+      
     <div className="flex flex-col gap-2 items-center justify-center min-h-screen">
-      <input type="text" placeholder="Usuario" className="border border-zinc-800 rounded-lg p-2" />
-      <input type="password" placeholder="Senha" className="border border-zinc-800 rounded-lg p-2" />
+      <input type="text" 
+             placeholder="Usuario"
+             value={usuario}
+             onChange={(e) =>setUsuario(e.target.value)} 
+             className="border border-zinc-800 rounded-lg p-2"
+              />
+      <input type="password" 
+             placeholder="Senha"
+             value={senha}
+             onChange={(e) =>setSenha(e.target.value)}
+             className="border border-zinc-800 rounded-lg p-2" 
+             />
       <div className="flex justify-center gap-2">
-        <button className="bg-blue-500 text-white rounded-lg p-2">Entrar</button>
+        <button onClick={handleEntrar} className="bg-blue-500 text-white rounded-lg p-2">Entrar</button>
         <Link
         href= "/cadastrar"
         className="bg-red-500 text-white rounded-lg p-2">
@@ -16,4 +47,5 @@ export default function Home() {
       </div>
     </div>
   );
+  handleEntrar()
 }
